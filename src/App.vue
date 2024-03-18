3fd7a18acd9c79ac5aafe9777a4355a5612ce471
Wind-Explorer <66894537+Wind-Explorer@users.noreply.github.com>
2024-03-19 01:33:35 +0800

    From zero

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <!-- HTML elements for the component -->
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <!-- HTML elements for the component -->
+    <h1>Hello!</h1>
+  </div>
+</template>
